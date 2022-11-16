6da515f70b6f11d12ba0d6157f3051650373b7b9
Deejai007 <105149434+Deejai007@users.noreply.github.com>
2022-11-16 23:10:52 +0530

    Committing files

---
+++ CodeChef/C++14/PRIMEDICE/80134778.cpp
@@ -0,0 +1,46 @@
+/*~CF~10/10/22:*/
+#include <bits/stdc++.h>
+#define ll long long
+#define rd(x) \
+    int x;    \
+    cin >> x
+#define read(x, y) \
+    int x, y;      \
+    cin >> x >> y
+#define all(x) (x).begin(), (x).end()
+#define get(a, n)               \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define fr(a, b) for (int i = a; i < b; i++)
+
+using namespace std;
+
+#define PI 3.1415926535897932384626433832795l
+const ll MOD = 1e9 + 7;
+
+void solve()
+{
+    read(a,b);
+    int x=a+b;
+    if(x==2||x==3||x==5||x==7||x==11)
+    {
+        cout<<"Alice"<<endl;
+    }
+    else cout<<"BOB"<<endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+
+        solve();
+    }
+}
