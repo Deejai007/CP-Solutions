2766521f14d682dc36e0167e2e791c6054793565
Deejai007 <105149434+Deejai007@users.noreply.github.com>
2022-11-21 19:11:24 +0530

    Committing files

---
+++ CodeForces/GNU C++17/160A | Twins/181826265.cpp
@@ -0,0 +1,69 @@
+/*~CF~10/10/22:*/
+#include <bits/stdc++.h>
+#define int long long
+#define rd(x) \
+    int x;    \
+    cin >> x
+#define read(x, y) \
+    int x, y;      \
+    cin >> x >> y
+#define all(x) (x).begin(), (x).end()
+#define get(a, n)               \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define fr(a, b) for (int i = a; i < b; i++)
+ 
+using namespace std;
+ 
+#define PI 3.1415926535897932384626433832795l
+const int MOD = 1e9 + 7;
+ 
+void solve()
+{
+ int n;
+ cin>>n;
+ int a[n];
+ int s[n];
+ for(int i=0;i<n;i++)
+ {
+  cin>>a[i];
+ 
+ }
+ sort(a,a+n);
+ s[0]=a[0];
+ 
+ for(int i=1;i<n;i++)
+ {
+  s[i]=s[i-1]+a[i];
+ }
+ 
+ for(int i=1;i<n;i++)
+ {
+  
+  int sm=s[n-1]-s[n-i-1];
+  if(sm>s[n-i-1])
+  {
+   cout<<i<<endl;
+   return;
+  }
+ }
+ 
+ cout<<n<<endl;
+}
+ 
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+ 
+        solve();
+    }
+}
