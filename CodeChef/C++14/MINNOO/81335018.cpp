3c8bc91923905926c7c6c09165424eb90abc02af
Deejai007 <105149434+Deejai007@users.noreply.github.com>
2022-12-01 00:17:39 +0530

    Committing files

---
+++ CodeChef/C++14/MINNOO/81335018.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--)
+	{
+	    int q;
+	    cin>>q;
+	    cout<<(q/2)<<endl;
+	    }
+	return 0;
+}
