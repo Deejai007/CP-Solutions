0ba4d1ed0d5f3a161772d72dd85034882ae5b956
Deejai007 <105149434+Deejai007@users.noreply.github.com>
2022-12-02 03:24:25 +0530

    Committing files

---
+++ CodeForces/GNU C++17/688B | Lovely Palindromes/183506207.cpp
@@ -0,0 +1,49 @@
+/*~CF~10/10/22:*/
+#include <bits/stdc++.h>
+#define int long long
+#define rd(x) \
+    int x;    \
+    cin >> x
+#define read(x, y) \
+    int x, y;      \
+    cin >> x >> y
+#define all(x) (x).begin(), (x).end()
+#define get(a, n)               \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define fr(a, b) for (int i = a; i < b; i++)
+ 
+using namespace std;
+ 
+#define PI 3.1415926535897932384626433832795l
+const int MOD = 1e9 + 7;
+ 
+void solve()
+{
+    // cout << "jkjdkjfjf" << endl;
+    string s;
+    cin >> s;
+    string k = s;
+    reverse(all(s));
+    cout << (k + s) << endl;
+}
+ 
+signed main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+ 
+        solve();
+    }
+}
