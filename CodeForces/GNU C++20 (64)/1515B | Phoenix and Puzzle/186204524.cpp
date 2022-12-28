f750d925cba5659223c039425a79730c61832e5e
Deejai007 <105149434+Deejai007@users.noreply.github.com>
2022-12-28 12:51:02 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1515B | Phoenix and Puzzle/186204524.cpp
@@ -0,0 +1,52 @@
+/*~CF~10/10/22:*/
+#include <bits/stdc++.h>
+#define int long long
+#define rd(x) \
+    int x;    \
+    cin >> x
+#define read(x, y) \
+    int x, y;      \
+    cin >> x >> y
+#define all(x) (x).begin(), (x).end()
+#define get(a, n)               \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define fr(a, b) for (int i = a; i < b; i++)
+ 
+using namespace std;
+ 
+#define PI 3.1415926535897932384626433832795l
+const int MOD = 1e9 + 7;
+bool isS(int x)
+{
+    int y = sqrt(x);
+    return y * y == x;
+}
+void solve()
+{
+ 
+    int n;
+    cin >> n;
+    if (n % 2 == 0 && isS(n / 2))
+        cout << "YES" << endl;
+    else if (n % 4 == 0 && isS(n / 4))
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+ 
+        solve();
+    }
+}
