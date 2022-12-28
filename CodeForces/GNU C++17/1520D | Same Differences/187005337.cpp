da47534424a6291ced3132f15058bffb2b69c782
Deejai007 <105149434+Deejai007@users.noreply.github.com>
2022-12-28 12:50:35 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1520D | Same Differences/187005337.cpp
@@ -0,0 +1,58 @@
+/*~CF~10/10/22:*/
+#include <bits/stdc++.h>
+#define int long long
+#define rd(x) \
+    int x;    \
+    cin >> x
+#define read(x, y) \
+    int x, y;      \
+    cin >> x >> y
+#define all(x) (x).begin(), (x).end()
+#define get(a, n)               \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define fr(a, b) for (int i = a; i < b; i++)
+ 
+using namespace std;
+ 
+#define PI 3.1415926535897932384626433832795l
+const int MOD = 1e9 + 7;
+ 
+void solve()
+{
+    rd(n);
+    int a[n];
+    get(a, n);
+    map<int, int> m;
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = a[i] - i;
+        m[a[i]]++;
+    }
+    int ans = 0;
+    for (auto i : m)
+    {
+        if (i.second > 1)
+        {
+            int tt = (i.second * (i.second - 1)) / 2;
+            ans += tt;
+        }
+    }
+    cout << ans << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+ 
+        solve();
+    }
+}
