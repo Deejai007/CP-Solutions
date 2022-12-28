cb4ea4ad1e2a29376140fe9806bcf0a185709530
Deejai007 <105149434+Deejai007@users.noreply.github.com>
2022-12-28 12:51:33 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1767A | Cut the Triangle/185515621.cpp
@@ -0,0 +1,55 @@
+/*~CF~10/10/22:*/
+#include <bits/stdc++.h>
+#define int long long
+#define rd(x) \
+    int x;    \
+    cin >> x
+#define read(x, y) \
+    int x, y;      \
+    cin >> x >> y
+#define all(x) (x).begin(), (x).end()
+#define get(a, n)               \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define fr(a, b) for (int i = a; i < b; i++)
+ 
+using namespace std;
+ 
+#define PI 3.1415926535897932384626433832795l
+const int MOD = 1e9 + 7;
+ 
+void solve()
+{
+ 
+    read(x1, y1);
+    read(x2, y2);
+    read(x3, y3);
+    if (x1 != x2 && x1 != x3 && x2 != x3)
+    {
+        cout << "YES" << endl;
+        return;
+    }
+    if (y1 != y3 && y1 != y2 && y2 != y3)
+    {
+        cout << "YES" << endl;
+        return;
+    }
+ 
+    cout << "NO" << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+ 
+        solve();
+    }
+}
