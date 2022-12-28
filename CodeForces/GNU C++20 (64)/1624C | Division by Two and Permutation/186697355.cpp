11e106b652c096e5a6b10430424eb4623c37e441
Deejai007 <105149434+Deejai007@users.noreply.github.com>
2022-12-28 12:50:51 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1624C | Division by Two and Permutation/186697355.cpp
@@ -0,0 +1,69 @@
+/*~CF~10/10/22:*/
+#include <bits/stdc++.h>
+#define int long long
+#define rd(x) \
+    int x;    \
+    cin >> x
+#define read(x, y) \
+    int x, y;      \
+    cin >> x >> y
+#define all(x) (x).begin(), (x).end()
+#define get(a, n)               \
+    int a[n];                   \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define fr(a, b) for (int i = a; i < b; i++)
+ 
+using namespace std;
+ 
+#define PI 3.1415926535897932384626433832795l
+const int MOD = 1e9 + 7;
+ 
+void solve()
+{
+    rd(n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    int pre[n + 1] = {0};
+    bool yy = true;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int num = a[i];
+        while (num > n || pre[num])
+        {
+            num /= 2;
+        }
+        if (num > 0)
+        {
+            pre[num] = true;
+        }
+        else
+            yy = false;
+    }
+ 
+    if (yy)
+ 
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+ 
+        solve();
+    }
+}
