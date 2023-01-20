73240e970713044aac96f894dd14b44b0f28580c
Deejai007 <105149434+Deejai007@users.noreply.github.com>
2023-01-21 03:53:23 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1541B | Pleasant Pairs/188784763.cpp
@@ -0,0 +1,52 @@
+/*~CF~10/10/22:*/
+#include <bits/stdc++.h>
+#define int long long
+#define rd(x) \
+    int x;    \
+    cin >> x
+#define read(x, y) \
+    int x, y;      \
+    cin >> x >> y
+#define all(x) (x).begin(), (x).end()
+#define get(a, n)               \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define fr(a, b) for (int i = a; i < b; i++)
+ 
+using namespace std;
+ 
+#define PI 3.1415926535897932384626433832795l
+const int MOD = 1e9 + 7;
+ 
+void solve()
+{
+    int n, ans = 0;
+    cin >> n;
+    int a[n + 1];
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        for (int j = 1; a[i] * j - i < i; j++)
+        {
+            if (a[i] * j - i >= 1 && a[a[i] * j - i] == j)
+                ans++;
+        }
+    }
+    cout << ans << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+ 
+        solve();
+    }
+}
