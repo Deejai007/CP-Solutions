a07fc95df0549143dae0a0f05aca563131c7a9e9
Deejai007 <105149434+Deejai007@users.noreply.github.com>
2023-05-18 01:39:02 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1786B | Cake Assembly Line/192522009.cpp
@@ -0,0 +1,61 @@
+/*~CF~10/10/22:*/
+#include <bits/stdc++.h>
+#define int long long
+#define rd(x) \
+    int x;    \
+    cin >> x
+#define read(x, y) \
+    int x, y;      \
+    cin >> x >> y
+#define all(x) (x).begin(), (x).end()
+#define get(a, n)               \
+    int a[n];                   \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define fr(a, b) for (int i = a; i < b; i++)
+ 
+using namespace std;
+ 
+#define PI 3.1415926535897932384626433832795l
+const int MOD = 1e9 + 7;
+ 
+void solve()
+{
+    int n, w, h;
+    cin >> n >> w >> h;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int b[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    int c[n];
+    for (int i = 0; i < n; i++)
+    {
+        c[i] = a[i] - b[i];
+    }
+    sort(c, c + n);
+    if (c[n - 1] - c[0] <= 2 * (w - h))
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+ 
+        solve();
+    }
+}
