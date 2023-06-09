e774267c3c568a8a74c330f22d6e42014de27aeb
Deejai007 <105149434+Deejai007@users.noreply.github.com>
2023-06-09 16:00:15 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1833A | Musical Puzzle/206466303.cpp
@@ -0,0 +1,52 @@
+/*~CF~10/10/22:*/
+#include <bits/stdc++.h>
+#define int long long
+#define rd(x) \
+    int x;    \
+    cin >> x
+#define read(x, y) \
+    int x, y;      \
+    cin >> x >> y
+#define all(x) (x).begin(), (x).end()
+#define get(a, n)               \
+    int a[n];                   \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> a[i];            \
+    }
+#define fr(a, b) for (int i = a; i < b; i++)
+ 
+using namespace std;
+ 
+#define PI 3.1415926535897932384626433832795l
+const int MOD = 1e9 + 7;
+ 
+void solve()
+{
+    rd(n);
+    string s;
+    cin >> s;
+    map<string, int> m;
+    for (int i = 0; i < n - 1; i++)
+    {
+        string s1 = "";
+        s1 += s[i];
+        s1 += s[i + 1];
+        m[s1]++;
+    }
+    cout << m.size() << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+ 
+        solve();
+    }
+}
