71a25e3448dabb2ad48ea5f044b3e9f523128849
Deejai007 <105149434+Deejai007@users.noreply.github.com>
2023-06-15 01:39:10 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1437B | Reverse Binary Strings/209598025.cpp
@@ -0,0 +1,55 @@
+/*~CF~10/10/22:*/
+#include <bits/stdc++.h>
+#define int long long
+#define rd(x) \
+    int x;    \
+    cin >> x
+#define read(x, y) \
+    int x, y;      \
+    cin >> x >> y
+#define all(x) (x).begin(), (x).end()
+#define get(a, n)                  \
+    int a[n];                      \
+    for (int gg = 0; gg < n; gg++) \
+    {                              \
+        cin >> a[i];               \
+    }
+#define fr(i, n) for (int i = 0; i < n; i++)
+ 
+using namespace std;
+ 
+#define PI 3.1415926535897932384626433832795l
+const int MOD = 1e9 + 7;
+ 
+void solve()
+{
+ 
+    int n, a = 0, b = 0;
+    cin >> n;
+    string s;
+    cin >> s;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '1' && s[i + 1] == '1')
+            a++;
+        else if (s[i] == '0' && s[i + 1] == '0')
+            b++;
+    }
+    cout << max(a, b);
+ 
+    cout << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+ 
+        solve();
+    }
+}
